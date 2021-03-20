655e7ac607542df7b2bd4cb2c806f3b3df5e85b4
Odanodlam <49739726+Odanodlam@users.noreply.github.com>
2021-03-19 21:22:47 -0300

    Criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package goarea
+
+import "math"
+
+//Pi é doido
+const Pi = 3.1416
+
+//Circ é responsável por calcular a área de um Círculo
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+//Rect é responsável por calular a área de um Retângulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
